bb5c5e16d59dc65df56a002f0f6e4507cd90714d
waynemorphic <wayneklyde@gmail.com>
2022-03-07 12:26:29 +0300

    Deleted unnecessary code in index.js

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 console.log("This is my js")
 let d = document;
 
-const maleName = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
-const femaleName = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
-const dd=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const mm=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-
-let maleGender = document.querySelector("#male");
-let femaleGender = document.querySelector("#female");
-let dateBirthday = document.querySelector("#birthday");
-
 
 
 function submitForm() {
@@ -36,13 +26,4 @@ let male="male"
     }
 }
 
-    //Ughhhhhh..no idea what i'm doing. So, I'll stop here
-   //  let inp = document.querySelector("#genderText");
-    //let h3 = document.querySelector("#outputText");
-
-    //function submitForm(){
-      //  let y = inp.value;
-        //if (y === ""){
-          //  h3.innerText = "Gender cannot be empty!";
-            //return;
-        //
+   
